83c43730acbd2a9a2aa92938e5004eeaa4326720
sahil dholpuria <dholpuria1999@gmail.com>
2021-05-14 22:33:44 +0530

    move to the folder

--- src/CLI_ipFinder.java
+++ src/CLI_ipFinder.java
@@ -1,3 +1,4 @@
+package src;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.*;
--- src/GUI_ipFinder.java
+++ src/GUI_ipFinder.java
@@ -1,3 +1,4 @@
+package src;
 public class GUI_ipFinder {
     public static void main(String[] args) {
         
