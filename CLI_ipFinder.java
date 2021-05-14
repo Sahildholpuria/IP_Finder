cbfa4501bd96b8267d677cf2e1b084f25f978aa1
sahil dholpuria <dholpuria1999@gmail.com>
2021-05-14 22:22:42 +0530

    rename the file

--- CLI_ipFinder.java
+++ CLI_ipFinder.java
@@ -1,12 +1,17 @@
-import java.lang.management.ThreadInfo;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.*;
 
 import javax.swing.JOptionPane;
 
-public class Main{
+public class CLI_ipFinder{
     public static void main(String[] args) throws UnknownHostException{
+
+        IP_Finder();
+        
+    }
+
+    public static void IP_Finder() throws UnknownHostException{
 
         Scanner sc = new Scanner(System.in); 
         String url = sc.nextLine().toLowerCase();
